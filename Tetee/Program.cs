e216bd91f7ff6fc8cf88ac326b0e794bf440addf
mbartkevicius <mariusbartkevicius@gmail.com>
2019-12-12 19:36:31 +0200

    main updated. Added two new methods.

--- Tetee/Program.cs
+++ Tetee/Program.cs
@@ -6,7 +6,25 @@ namespace Tetee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 5;
+            Console.WriteLine("start :" + number);
+            Kazkas(number);
+            Console.WriteLine("after normal :" + number);
+            Kazkas2(ref number);
+            Console.WriteLine("after ref :" + number);
+            Kazkas3(out number);
+            Console.WriteLine("after out :" + number);
+
         }
+
+
+        static int Kazkas(int sk)
+        {
+            sk = sk * 2;
+            return sk;
+
+        }
+
+
     }
 }
